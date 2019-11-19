0d6c4897340a70f202ac252d7b7b6209657b3eea
Andrew Morozko <Andrew-Morozko@users.noreply.github.com>
2019-11-19 23:42:13 +0300

    Removed unneeded pointers

--- mbpdecor/mbpdecor.go
+++ mbpdecor/mbpdecor.go
@@ -18,26 +18,26 @@ func Status(status string, wcc ...decor.WC) *StatusDecorator {
 	}
 	wc.Init()
 	d := &StatusDecorator{
-		WC:     wc,
-		status: &status,
+		WC:     wc.Init(),
+		status: status,
 	}
 	return d
 }
 
 type StatusDecorator struct {
 	decor.WC
-	status     *string
+	status     string
 	statusLock sync.Mutex
 }
 
 func (d *StatusDecorator) SetStatus(status string) {
 	d.statusLock.Lock()
 	defer d.statusLock.Unlock()
-	d.status = &status
+	d.status = status
 }
 
 func (d *StatusDecorator) Decor(st *decor.Statistics) string {
 	d.statusLock.Lock()
 	defer d.statusLock.Unlock()
-	return d.FormatMsg(*d.status)
+	return d.FormatMsg(d.status)
 }
